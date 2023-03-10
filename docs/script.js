25bcd65cac2244bf0130f8bda2a69a1da9146ff3
YenlikCurse <124899430+YenlikCurse@users.noreply.github.com>
2023-03-09 21:16:39 -0500

    2.0
    Nouvel Interface

--- docs/script.js
+++ docs/script.js
@@ -1,21 +1,31 @@
-(function() {
-const squares = document.querySelectorAll('.square');
-    
-squares.forEach((square) => {
-  const rectangle = square.querySelector('.rectangle');
-
-  rectangle.style.display = 'none';
-
-  square.addEventListener('mouseover', () => {
-    rectangle.style.display = 'block';
-  });
-
-  square.addEventListener('mouseout', () => {
-    rectangle.style.display = 'none';
-  });
+(function iifeClickEventListenerToCircles() {
+/* Declare circle and rectangle and add an event listener to the circle to make the rectangle visible, using the class="show", on mouse click */
+const purple = document.getElementById('purple');
+const purpleRectangle = document.getElementById('purpleRectangle');
+purple.addEventListener('click', () => {
+  purpleRectangle.classList.toggle('show');
+  purple.classList.toggle('circleWhileShown');
 });
-}());
-
-
-
+/* Declare circle and rectangle and add an event listener to the circle to make the rectangle visible, using the class="show", on mouse click */
+const green = document.getElementById('green');
+const greenRectangle = document.getElementById('greenRectangle');
+green.addEventListener('click', () => {
+  greenRectangle.classList.toggle('show');
+  green.classList.toggle('circleWhileShown');
+});
+/* Declare circle and rectangle and add an event listener to the circle to make the rectangle visible, using the class="show", on mouse click */
+const red = document.getElementById('red');
+const redRectangle = document.getElementById('redRectangle');
+red.addEventListener('click', () => {
+  redRectangle.classList.toggle('show');
+  red.classList.toggle('circleWhileShown');
+});
+/* Declare circle and rectangle and add an event listener to the circle to make the rectangle visible, using the class="show", on mouse click */
+const turquoise = document.getElementById('turquoise');
+const turquoiseRectangle = document.getElementById('turquoiseRectangle');
+turquoise.addEventListener('click', () => {
+    turquoiseRectangle.classList.toggle('show');
+    purple.classList.toggle('circleWhileShown');
+});
+})();
 
