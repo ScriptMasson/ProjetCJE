7bb6480ca90606fabb6c2dd6c5c6db3b4613b49c
YenlikCurse <124899430+ScriptMasson@users.noreply.github.com>
2023-03-13 13:30:07 -0400

    2.2
    Arrival animations and eventlisteners

--- docs/script.js
+++ docs/script.js
@@ -1,31 +1,60 @@
 (function iifeClickEventListenerToCircles() {
 /* Declare circle and rectangle and add an event listener to the circle to make the rectangle visible, using the class="show", on mouse click */
-const purple = document.getElementById('purple');
-const purpleRectangle = document.getElementById('purpleRectangle');
+let purple = document.getElementById('purple');
+let purpleRectangle = document.getElementById('purpleRectangle');
 purple.addEventListener('click', () => {
   purpleRectangle.classList.toggle('show');
   purple.classList.toggle('circleWhileShown');
 });
 /* Declare circle and rectangle and add an event listener to the circle to make the rectangle visible, using the class="show", on mouse click */
-const green = document.getElementById('green');
-const greenRectangle = document.getElementById('greenRectangle');
+let green = document.getElementById('green');
+let greenRectangle = document.getElementById('greenRectangle');
 green.addEventListener('click', () => {
   greenRectangle.classList.toggle('show');
   green.classList.toggle('circleWhileShown');
 });
 /* Declare circle and rectangle and add an event listener to the circle to make the rectangle visible, using the class="show", on mouse click */
-const red = document.getElementById('red');
-const redRectangle = document.getElementById('redRectangle');
+let red = document.getElementById('red');
+let redRectangle = document.getElementById('redRectangle');
 red.addEventListener('click', () => {
   redRectangle.classList.toggle('show');
   red.classList.toggle('circleWhileShown');
 });
 /* Declare circle and rectangle and add an event listener to the circle to make the rectangle visible, using the class="show", on mouse click */
-const turquoise = document.getElementById('turquoise');
-const turquoiseRectangle = document.getElementById('turquoiseRectangle');
+let turquoise = document.getElementById('turquoise');
+let turquoiseRectangle = document.getElementById('turquoiseRectangle');
 turquoise.addEventListener('click', () => {
     turquoiseRectangle.classList.toggle('show');
     turquoise.classList.toggle('circleWhileShown');
 });
 })();
+
+(function iifeArrivalOfBubbles() {
+
+  setTimeout(purpleArrival, 1000);
+  setTimeout(greenArrival, 2000);
+  setTimeout(redArrival, 3000);
+  setInterval(turquoiseArrival, 4000);
+
+  function purpleArrival() {
+    const purple = document.getElementById('purple');
+    purple.classList.remove('loadAnimated');
+  };
+
+  function greenArrival() {
+    const green = document.getElementById('green');
+    green.classList.remove('loadAnimated');
+  };
+
+  function redArrival() {
+    const red = document.getElementById('red');
+    red.classList.remove('loadAnimated');
+  };
+
+  function turquoiseArrival() {
+    const turquoise = document.getElementById('turquoise');
+    turquoise.classList.remove('loadAnimated');
+  };
+})();
+
 
